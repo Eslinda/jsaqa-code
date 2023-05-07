11f448cc0d363e1b3703a0f1bf5fa259a9ae59b3
Tatiana Lyapina <antipts@gmail.com>
2023-05-07 21:24:04 +0300

    Add task Jest

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,31 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+    const result = sorting.sortByName(input);
+
+    expect(result).toEqual(expected);
+  });
+
+  it("Book name already arranged in ascending order", () => {
+    const input = [
+      "Властелин Колец",
+      "Властелин Колец",
+      "Властелин Колец",
+    ];
+    const result = sorting.sortByName(input);
+
+    expect(result).toEqual(input);
   });
 });
